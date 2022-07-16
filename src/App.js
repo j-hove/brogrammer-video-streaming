fab4ae3e8f211c2416587ca84efc0e6aaa5ff01e
Jhoven Español <jhoven.espanol@gmail.com>
2022-07-16 21:28:43 +0800

    added Login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; // (I think this is for version react-router-dom v5)
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>
-        Video-Streaming-App Project
-      </h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      {/* (I think this is for version react-router-dom v5) */}
+      {/* <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router> */}
     </div>
   );
 }
