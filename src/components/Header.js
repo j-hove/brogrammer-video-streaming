65fb522b5d7231a2285d7965b143415b777e398f
Jhoven Español <jhoven.espanol@gmail.com>
2022-08-04 18:30:30 +0800

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,234 +1,319 @@
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+// import styled from "styled-components";
+// import { useNavigate } from "react-router-dom";
+import "./Header.css"
+import React, { Component } from 'react';
+import { Navigate } from "react-router-dom";
 
-const Header = (props) => {
-    let navigate = useNavigate();
-    const pathname = window.location.pathname;   
 
-    return (
-        <Nav>
-            <Logo>
-                <img src="/images/logo.svg" alt="Disney+ Logo" />
-            </Logo>
-            {pathname == "/brogrammer-video-streaming/" ? (
-                <Login 
-                    onClick={() => {
-                        navigate("/brogrammer-video-streaming/login");
-                        }}
-                    >
-                    LOGIN
-                </Login>
-            ) : (
-                <>
-                    <NavMenu>
-                        <a href="/brogrammer-video-streaming/home">
-                            <img src="/images/home-icon.svg" alt="HOME" />   
-                            <span>
-                                HOME
-                            </span>                 
-                        </a>
-                        <a>
-                            <img src="/images/search-icon.svg" alt="SEARCH" />   
-                            <span>
-                                SEARCH
-                            </span>                 
-                        </a> 
-                        <a>
-                            <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />   
-                            <span>
-                                WATCHLIST
-                            </span>                 
-                        </a> 
-                        <a>
-                            <img src="/images/original-icon.svg" alt="ORIGINALS" />   
-                            <span>
-                                ORIGINALS
-                            </span>                 
-                        </a> 
-                        <a>
-                            <img src="/images/movie-icon.svg" alt="MOVIES" />   
-                            <span>
-                                MOVIES
-                            </span>                 
-                        </a>
-                        <a>
-                            <img src="/images/series-icon.svg" alt="SERIES" />   
-                            <span>
-                                SERIES
-                            </span>                 
-                        </a>                    
-                    </NavMenu>
-                    {/* UserMenu component is actually SignOut component                     */}
-                    <UserMenu> 
-                        <UserImg src="https://featuredanimation.com/wp-content/uploads/2022/02/Loki-Disney-Plus-Icon.jpg.webp" alt="user" />                
-                        <DropDown>
-                            <span onClick={() => {
-                                navigate("/brogrammer-video-streaming/main");
-                                }}
-                            >Sign Out</span>
-                        </DropDown>
-                    </UserMenu>
-                </>
-            )}          
-        </Nav>
-    
-    );
-};
+class Header extends Component {
+    constructor(props){
+        super(props);
+        this.pathname = window.location.pathname;
+        this.redirect = false;
+    }   
 
-const Nav = styled.nav`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 70px;
-    background-color: #090b13;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 36px;
-    letter-spacing: 16px;
-    z-index: 3;
-`;
+    // state = {
+    //     redirect:false
+    // }  
 
-const Logo = styled.a`
-    padding: 0;
-    margin-top: 4px;
-    width: 80px;
-    max-height: 70px;
-    font-size: 0;
-    display: inline-block;
-
-    img {
-        display: block;
-        width: 100%;
+    render () {
+        return (
+            <nav className="Nav">
+                <a href="/home" className="Logo">
+                    <img src="/images/logo.svg" alt="Disney+ Logo" />
+                </a>
+                {this.pathname === "/brogrammer-video-streaming" ? (
+                    <button className="Login"
+                        onClick={ 
+                            this.redirect && <Navigate to='/brogrammer-video-streaming/login' replace={true}/>
+                        }>
+                        LOGIN
+                    </button>
+                ) : (
+                    <>
+                        <div className="NavMenu">
+                            <a href="/brogrammer-video-streaming/home">
+                                <img src="/images/home-icon.svg" alt="HOME" />   
+                                <span>
+                                    HOME
+                                </span>                 
+                            </a>
+                            <a href="/home">
+                                <img src="/images/search-icon.svg" alt="SEARCH" />   
+                                <span>
+                                    SEARCH
+                                </span>                 
+                            </a> 
+                            <a href="/home">
+                                <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />   
+                                <span>
+                                    WATCHLIST
+                                </span>                 
+                            </a> 
+                            <a href="/home">
+                                <img src="/images/original-icon.svg" alt="ORIGINALS" />   
+                                <span>
+                                    ORIGINALS
+                                </span>                 
+                            </a> 
+                            <a href="/home">
+                                <img src="/images/movie-icon.svg" alt="MOVIES" />   
+                                <span>
+                                    MOVIES
+                                </span>                 
+                            </a>
+                            <a href="/home">
+                                <img src="/images/series-icon.svg" alt="SERIES" />   
+                                <span>
+                                    SERIES
+                                </span>                 
+                            </a>                    
+                        </div>
+                        {/* UserMenu component is actually SignOut component                     */}
+                        <div className="UserMenu"> 
+                            <img className="UserImg" src="https://featuredanimation.com/wp-content/uploads/2022/02/Loki-Disney-Plus-Icon.jpg.webp" alt="user" />                
+                            <div className="DropDown">
+                                <span
+                                    onClick={ 
+                                        this.redirect && <Navigate to='/brogrammer-video-streaming' replace={true}/>
+                                    }
+                                >Sign Out</span>
+                            </div>
+                        </div>
+                    </>
+                )}          
+            </nav>
+        )
     }
-`;
+}
 
-const NavMenu = styled.div`
-    align-items: center;
-    display: flex;
-    flex-flow: row nowrap;
-    height: 100%;
-    justify-content: flex-end;
-    margin: 0px;
-    padding: 0px;
-    position: relative;
-    margin-right: auto;
-    margin-left: 25px;    
+// const Header = (props) => {
+    // let navigate = useNavigate();
+    // const pathname = window.location.pathname;   
 
-    a {
-        display: flex;
-        align-items: center;
-        padding: 0 12px;
+//     return (
+//         <Nav>
+//             <Logo>
+//                 <img src="/images/logo.svg" alt="Disney+ Logo" />
+//             </Logo>
+//             {pathname == "/" ? (
+//                 <Login 
+//                     onClick={() => {
+//                         navigate("/login");
+//                         }}
+//                     >
+//                     LOGIN
+//                 </Login>
+//             ) : (
+//                 <>
+//                     <NavMenu>
+//                         <a href="/home">
+//                             <img src="/images/home-icon.svg" alt="HOME" />   
+//                             <span>
+//                                 HOME
+//                             </span>                 
+//                         </a>
+//                         <a>
+//                             <img src="/images/search-icon.svg" alt="SEARCH" />   
+//                             <span>
+//                                 SEARCH
+//                             </span>                 
+//                         </a> 
+//                         <a>
+//                             <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />   
+//                             <span>
+//                                 WATCHLIST
+//                             </span>                 
+//                         </a> 
+//                         <a>
+//                             <img src="/images/original-icon.svg" alt="ORIGINALS" />   
+//                             <span>
+//                                 ORIGINALS
+//                             </span>                 
+//                         </a> 
+//                         <a>
+//                             <img src="/images/movie-icon.svg" alt="MOVIES" />   
+//                             <span>
+//                                 MOVIES
+//                             </span>                 
+//                         </a>
+//                         <a>
+//                             <img src="/images/series-icon.svg" alt="SERIES" />   
+//                             <span>
+//                                 SERIES
+//                             </span>                 
+//                         </a>                    
+//                     </NavMenu>
+//                     {/* UserMenu component is actually SignOut component                     */}
+//                     <UserMenu> 
+//                         <UserImg src="https://featuredanimation.com/wp-content/uploads/2022/02/Loki-Disney-Plus-Icon.jpg.webp" alt="user" />                
+//                         <DropDown>
+//                             <span onClick={() => {
+//                                 navigate("/");
+//                                 }}
+//                             >Sign Out</span>
+//                         </DropDown>
+//                     </UserMenu>
+//                 </>
+//             )}          
+//         </Nav>
+//     );
+// };
+
+// const Nav = styled.nav`
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     right: 0;
+//     height: 70px;
+//     background-color: #090b13;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 0 36px;
+//     letter-spacing: 16px;
+//     z-index: 3;
+// `;
+
+// const Logo = styled.a`
+//     padding: 0;
+//     margin-top: 4px;
+//     width: 80px;
+//     max-height: 70px;
+//     font-size: 0;
+//     display: inline-block;
+
+//     img {
+//         display: block;
+//         width: 100%;
+//     }
+// `;
+
+// const NavMenu = styled.div`
+//     align-items: center;
+//     display: flex;
+//     flex-flow: row nowrap;
+//     height: 100%;
+//     justify-content: flex-end;
+//     margin: 0px;
+//     padding: 0px;
+//     position: relative;
+//     margin-right: auto;
+//     margin-left: 25px;    
+
+//     a {
+//         display: flex;
+//         align-items: center;
+//         padding: 0 12px;
     
-        img {
-          height: 20px;
-          min-width: 20px;
-          width: 20px;
-          z-index: auto;
-        }
+//         img {
+//           height: 20px;
+//           min-width: 20px;
+//           width: 20px;
+//           z-index: auto;
+//         }
     
-        span {
-          color: rgb(249, 249, 249);
-          font-size: 13px;
-          letter-spacing: 1.42px;
-          line-height: 1.08;
-          padding: 2px 0px;
-          white-space: nowrap;
-          position: relative;
+//         span {
+//           color: rgb(249, 249, 249);
+//           font-size: 13px;
+//           letter-spacing: 1.42px;
+//           line-height: 1.08;
+//           padding: 2px 0px;
+//           white-space: nowrap;
+//           position: relative;
     
-          &:before {
-            background-color: rgb(249, 249, 249);
-            border-radius: 0px 0px 4px 4px;
-            bottom: -6px;
-            content: "";
-            height: 2px;
-            left: 0px;
-            opacity: 0;
-            position: absolute;
-            right: 0px;
-            transform-origin: left center;
-            transform: scaleX(0);
-            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-            visibility: hidden;
-            width: auto;
-          }
-        }
+//           &:before {
+//             background-color: rgb(249, 249, 249);
+//             border-radius: 0px 0px 4px 4px;
+//             bottom: -6px;
+//             content: "";
+//             height: 2px;
+//             left: 0px;
+//             opacity: 0;
+//             position: absolute;
+//             right: 0px;
+//             transform-origin: left center;
+//             transform: scaleX(0);
+//             transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+//             visibility: hidden;
+//             width: auto;
+//           }
+//         }
     
-        &:hover {
-          span:before {
-            transform: scaleX(1);
-            visibility: visible;
-            opacity: 1 !important;
-          }
-        }
-      }    
+//         &:hover {
+//           span:before {
+//             transform: scaleX(1);
+//             visibility: visible;
+//             opacity: 1 !important;
+//           }
+//         }
+//       }    
 
-    @media (max-width: 768px) {
-        display: none;
-    }
-`;
+//     @media (max-width: 768px) {
+//         display: none;
+//     }
+// `;
 
-const Login = styled.button`
-    background-color: rgba(0,0,0,0.6);
-    color: #f9f9f9;
-    padding: 8px 16px;
-    text-transform: uppercase;
-    letter-spacing: 1.5px;
-    border: 1px solid #f9f9f9;
-    border-radius: 4px;
-    transition: all 0.2s ease 0s;
-    cursor: pointer;
+// const Login = styled.button`
+//     background-color: rgba(0,0,0,0.6);
+//     color: #f9f9f9;
+//     padding: 8px 16px;
+//     text-transform: uppercase;
+//     letter-spacing: 1.5px;
+//     border: 1px solid #f9f9f9;
+//     border-radius: 4px;
+//     transition: all 0.2s ease 0s;
+//     cursor: pointer;
 
-    &:hover {
-        background-color: #f9f9f9;
-        color: #000;
-        border-color: transparent;
-    };
-`;
+//     &:hover {
+//         background-color: #f9f9f9;
+//         color: #000;
+//         border-color: transparent;
+//     };
+// `;
 
-const UserImg = styled.img`
-    height: 100%;
-`;
+// const UserImg = styled.img`
+//     height: 100%;
+// `;
 
-const DropDown = styled.div`
-    position: absolute;
-    top: 48px;
-    right: 0px;
-    background: rgb(19, 19, 19);
-    border: 1px solid rgba(151, 151, 151, 0.34);
-    border-radius: 4px;
-    box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
-    padding: 10px;
-    font-size: 14px;
-    letter-spacing: 3px;
-    width: 100px;
-    opacity: 0;
-`;
+// const DropDown = styled.div`
+//     position: absolute;
+//     top: 48px;
+//     right: 0px;
+//     background: rgb(19, 19, 19);
+//     border: 1px solid rgba(151, 151, 151, 0.34);
+//     border-radius: 4px;
+//     box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+//     padding: 10px;
+//     font-size: 14px;
+//     letter-spacing: 3px;
+//     width: 100px;
+//     opacity: 0;
+// `;
 
-const UserMenu = styled.div`
-    position: relative;
-    height: 48px;
-    width: 48px;
-    display: flex;
-    cursor: pointer;
-    align-items: center;
-    justify-content: center;
+// const UserMenu = styled.div`
+//     position: relative;
+//     height: 48px;
+//     width: 48px;
+//     display: flex;
+//     cursor: pointer;
+//     align-items: center;
+//     justify-content: center;
 
-    ${UserImg} {
-        border-radius: 50%;
-        width: 100%;
-        height: 100%;
-    }
+//     ${UserImg} {
+//         border-radius: 50%;
+//         width: 100%;
+//         height: 100%;
+//     }
 
-    &:hover {
-        ${DropDown} {
-            opacity: 1;
-            transition-duration: 1s;
-        }
-    }
+//     &:hover {
+//         ${DropDown} {
+//             opacity: 1;
+//             transition-duration: 1s;
+//         }
+//     }
 
-`;
+// `;
 
 
 
